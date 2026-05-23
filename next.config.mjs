9b62a4bce0c011c6584c8e72d48c08f1276be7cc
Zhrzhr77 <3241714867@qq.com>
2026-05-23 17:04:16 +0800

    添加 Cloudflare Pages 部署配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
