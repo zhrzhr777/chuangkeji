9e23d2d78c3ec6a53267523aeb6d2272d1c9143e
Zhrzhr77 <3241714867@qq.com>
2026-05-23 17:15:07 +0800

    移除 Cloudflare 配置，适配 Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,9 +5,5 @@ const nextConfig = {
   },
   eslint: { ignoreDuringBuilds: true },
 };
-
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
